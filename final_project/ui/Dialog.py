134c0b900b1212d75c11a11017630e5d066f1e1c
40623133 <40623133@gm.nfu.edu.tw>
2017-12-28 10:03:59 +0800

    backspace

--- final_project/ui/Dialog.py
+++ final_project/ui/Dialog.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtWidgets import QDialog
+from .Ui_Dialog import Ui_Dialog
 
 class Dialog(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
-
-        '''以下為使用者自行編寫程式碼區'''
-        self.display.setText('0')
-        digits = [self.zero, self.one, self.two,\
-        self.three, self.four, self.five, self.six, \
-        self.seven, self.eight, self.nine ]
-        for i in digits:
-            i.clicked.connect(self.digitClicked)
-        self.clearAllButton.clicked.connect(self.clearAll) 
-        
-        
-
-
         self.waitingForOperand = True
         #數字鈕連接函式 (Function)
         for button in [
@@ -72,46 +60,6 @@ class Dialog(QDialog, Ui_Dialog):
         #顯示數字
         self.display.setText(self.display.text() + str(digitValue))
         
-        
-
-    def unaryOperatorClicked(self):
-        '''單一運算元按下後處理方法'''
-        pass
-        
-    def additiveOperatorClicked(self):
-        '''加或減按下後進行的處理方法'''
-        pass
-        
-    def multiplicativeOperatorClicked(self):
-        '''乘或除按下後進行的處理方法'''
-        pass
-        
-    def equalClicked(self):
-        '''等號按下後的處理方法'''
-        pass
-        
-    def pointClicked(self):
-        '''小數點按下後的處理方法'''
-        
-        
-    def changeSignClicked(self):
-        '''變號鍵按下後的處理方法'''
-        #pass
-        if self.wait:
-             self.display.setText('0')
-  
-        if "." not in self.display.text():
-             self.display.setText(self.display.text() + ".")
-  
-        self.wait = False
-        
-    def backspaceClicked(self):
-        '''回復鍵按下的處理方法'''
-        pass
-        
-
-    
-
     def clear(self):
         if self.waitingForOperand:
             return
